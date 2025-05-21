52bfa029a1a87e19bc4b707c210265e583389f3b
吳元皓 <hw@yuanhau.com>
2025-05-21 11:11:50 +0800

    A basic chrome ext app. Change to Vite.

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const serverInput = ref();
+</script>
+
+<template>
+    <div class="justify-center align-center text-center p-4">
+        <div class="flex flex-row">
+            <span>Current: </span><span>{{ serverInput }}</span>
+        </div>
+        <h3 class="text-2xl text-bold">Change your server url:</h3>
+        <div class="flex flex-row">
+            <input type="text" v-model="serverInput" />
+            <button>Submit</button>
+        </div>
+    </div>
+</template>
